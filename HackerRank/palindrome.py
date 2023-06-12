9a9c90ccecb040852d1c93c46b2a82c41ac615a9
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2023-06-12 17:14:37 +0000

    Delete palindrome.py

--- HackerRank/palindrome.py
+++
@@ -1,7 +0,0 @@
-https://leetcode.com/problems/palindrome-number/description/
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if str(x) == str(x)[::-1]:
-            return True
-        else:
-            return False

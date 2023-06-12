970f01b8001ed7f4650f283b220465f83c4dd900
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2023-06-12 17:16:15 +0000

    Create palindrome.py

---
+++ LeetCode/palindrome.py
@@ -0,0 +1,9 @@
+
+
+https://leetcode.com/problems/palindrome-number/description/
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return False

0d61640afd3aef1d9f414f8b14eb59bf894d83a8
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2023-06-12 17:13:25 +0000

    Delete convert_temp.py

--- HackerRank/convert_temp.py
+++
@@ -1,6 +0,0 @@
-
-https://leetcode.com/problems/convert-the-temperature/description/
-
-class Solution:
-    def convertTemperature(self, celsius: float) -> List[float]:
-        return [celsius + 273.15, (celsius * 1.80) + 32.00] 

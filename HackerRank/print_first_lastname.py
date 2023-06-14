ec8b54ca8a88708fb3ca6825e603f60d62d69aa7
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2023-06-14 15:17:22 +0000

    Create print_first_lastname.py

---
+++ HackerRank/print_first_lastname.py
@@ -0,0 +1,5 @@
+https://www.hackerrank.com/challenges/whats-your-name/problem?isFullScreen=true
+
+def print_full_name(first, last):
+    print('Hello '  + first + ' ' + last +'! ' +'You just delved into python.')
+    

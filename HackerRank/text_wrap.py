96cde8f0a93c86e96149e76ce16f21ff79ab1793
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2023-06-24 14:45:57 +0000

    Update text_wrap.py

--- HackerRank/text_wrap.py
+++ HackerRank/text_wrap.py
@@ -1,1 +1,11 @@
 
+https://www.hackerrank.com/challenges/text-wrap/problem?isFullScreen=true
+import textwrap
+
+def wrap(string, max_width):
+    return textwrap.fill(string, max_width)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)

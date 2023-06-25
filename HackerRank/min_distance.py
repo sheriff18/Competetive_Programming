cbfef1090f89ddfb2354777e7019554f476d5aaa
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2023-06-25 23:43:24 +0000

    Create min_distance.py

---
+++ HackerRank/min_distance.py
@@ -0,0 +1,10 @@
+class Solution:
+    def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
+
+        mindist, ans = float("inf"), -1
+        for i, (a, b) in enumerate(points):
+            if a == x or b == y:
+                dist = abs(a - x) + abs(b - y)
+                if dist < mindist:
+                    ans, mindist = i, dist
+        return ans

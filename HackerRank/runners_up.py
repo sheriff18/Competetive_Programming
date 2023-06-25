8d1d4645dc56ed6ea900374719679af927c83052
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2023-06-25 22:55:35 +0000

    Create runners_up.py

---
+++ HackerRank/runners_up.py
@@ -0,0 +1,7 @@
+https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+res = sorted(set(arr))
+print(res[-2])
